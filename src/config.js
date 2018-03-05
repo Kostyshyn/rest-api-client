5e7f1620920d72ce998ed7895b7494fb6491eb6a
Kostyshyn <kostyshyn@ua.fm>
2018-03-05 18:16:50 -0500

    [~] chat template

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-var HOST = 'http://192.168.0.102'; // local ip addr 
+var HOST = 'http://192.168.1.101'; // local ip addr 
 // var HOST = 'http://localhost';
 export const ROOT_URI = HOST + ':8889';
 console.log(ROOT_URI)
