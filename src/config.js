e2f330208d978ad057c00765f0a35848c08ff378
Kostyshyn <kostyshyn@ua.fm>
2018-03-18 13:51:32 -0400

    fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// var HOST = 'http://192.168.0.119'; // local ip addr 
-var HOST = 'http://localhost';
+var HOST = 'http://192.168.1.105'; // local ip addr 
+// var HOST = 'http://localhost';
 export const ROOT_URI = HOST + ':8889';
 console.log(ROOT_URI)
